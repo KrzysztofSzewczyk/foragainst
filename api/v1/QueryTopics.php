6dd56cd41c9adbfe2fd096235afc86b3b96001a6
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2019-06-01 15:10:53 +0200

    Database initialization code

--- api/v1/QueryTopics.php
+++ api/v1/QueryTopics.php
@@ -1,5 +1,19 @@
+
 <?php
 
+require 'vendor/autoload.php';
 
+header('Content-Type: application/json');
+
+$database = new \Filebase\Database([
+    'dir' => 'db/'
+]);
+
+$json = "{}";
+
+if(!isset($_GET['query'])) {
+	echo "{\"error\": \"Query string missing\"}";
+	die;
+}
 
 ?>
