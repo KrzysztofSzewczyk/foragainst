588224032d93f327eae24582d484b5af5bec4b16
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2019-06-01 15:14:24 +0200

    Amount and page ID parameters

--- api/v1/QueryTopics.php
+++ api/v1/QueryTopics.php
@@ -11,9 +11,19 @@ $database = new \Filebase\Database([
 
 $json = "{}";
 
+$req_amount = 10;
+$req_offset = 0;
+
 if(!isset($_GET['query'])) {
 	echo "{\"error\": \"Query string missing\"}";
 	die;
 }
+
+if(isset($_GET['amount']))
+	$req_amount = intval($_GET['amount']);
+
+if(isset($_GET['page']))
+	$req_offset = intval($_GET['page']) * $req_amount;
+
 
 ?>
