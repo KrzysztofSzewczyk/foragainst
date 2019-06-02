a08531561c1a1a3e87db97581226443275e63d05
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2019-06-02 16:30:14 +0200

    topic.Query seems unfixable currently

--- api/v1/topic/Query.php
+++ api/v1/topic/Query.php
@@ -15,7 +15,7 @@ $req_amount = 10;
 $req_offset = 0;
 
 if(!isset($_GET['query'])) {
-	echo "{\"error\": \"Query string missing\"}";
+	echo json_encode(array("error" => "Query string missing."));
 	die;
 }
 
@@ -26,6 +26,9 @@ if(isset($_GET['page']))
 	$req_offset = intval($_GET['page']) * $req_amount;
 
 $results = $database->where('title','REGEX',$_GET['query'])->andWhere('introduced','=',true)->limit($req_amount, $req_offset)->results();
+
+// Note: It seems like generating JSON by hand is the simplest and the most efficent way to do it, yet
+//       there is slight chance I'm wrong. When better workaround comes in place, delete this clusterfuck of code.
 
 $json .= "{\"result\": [";
 
