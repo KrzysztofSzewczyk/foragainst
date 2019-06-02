d985a72a6f7b648d08780b4fb4e7b44a2269e408
Krzysztof Szewczyk <marekszuwarek1958@gmail.com>
2019-06-02 15:54:57 +0200

    Topic introducing ratelimiting

--- api/v1/topic/Introduce.php
+++ api/v1/topic/Introduce.php
@@ -7,6 +7,18 @@ $database = new \Filebase\Database([
     'dir' => 'db/topic/'
 ]);
 
+session_start();
+if (isset($_SESSION['LAST_CALL'])) {
+	$last = strtotime($_SESSION['LAST_CALL']);
+	$curr = strtotime(date("Y-m-d h:i:s"));
+	$sec = abs($last - $curr);
+	if ($sec <= 60 * 20) { // You can post argument every 20 minutes.
+		echo "{\"error\": \"Ratelimit exceeded.\"}";
+		die;   
+	}
+}
+$_SESSION['LAST_CALL'] = date("Y-m-d h:i:s");
+
 if(!isset($_POST['title']) && 
    !isset($_POST['description'])) {
 	echo "{\"error\": \"Title and description missing\"}";
